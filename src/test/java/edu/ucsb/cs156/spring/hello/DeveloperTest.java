f62db52b9eb8af7eeb3d84691fdc73f08153c033
yarwinliu <yarwinliu@ucsb.edu>
2025-04-07 22:31:49 -0700

    yl - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -22,12 +22,14 @@ public class DeveloperTest {
 
     @Test
     public void getName_returns_correct_name() {
-        // Replace Chris G. with your name as shown on
-        // <https://bit.ly/cs156-s25-teams>
         assertEquals("Yarwin L.", Developer.getName());
     }
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("yarwinliu", Developer.getGithubId());
+    }
 
 }
