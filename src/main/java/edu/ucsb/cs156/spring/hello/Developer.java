5145f3e7bc568526c5412cdc6ac6ab5f21f330e2
yarwinliu <yarwinliu@ucsb.edu>
2025-04-07 22:27:29 -0700

    updated with actual dev name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,7 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-s25-teams>
-        // i.e. your first name, or your first and initial of last name
-
-        return "Chris G.";
+        return "Yarwin L.";
     }
 
     /**
@@ -29,8 +25,7 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
-        return "cgaucho";
+        return "yarwinliu";
     }
 
     /**
@@ -39,14 +34,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s25-06");
+        team.addMember("Aryan");
+        team.addMember("Rachit");
+        team.addMember("Riya");
+        team.addMember("Saeed");
+        team.addMember("Sanjana");
+        team.addMember("Yarwin");
         return team;
     }
 }
